991efff6c10fec7dce9ffc9488cce4349c0999ab
Jack Butler <jackbutler.info@gmail.com>
2025-04-30 22:24:29 +0100

    setting up the DOM for a modal form and storing each todo item

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 import "./styles.css";
+import { createItemModal } from "./render";
+
+class TodoItem {
+  constructor(id, title, description, dueDate, priority, notes) {
+    this.id = id;
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+    this.notes = notes;
+  }
+}
+
+const FirstTodoItem = new TodoItem(
+  "Buy shoes",
+  "I am hoping to buy shoes",
+  "01/05/2025"
+);
+
+console.log(FirstTodoItem);
+
+createItemModal();
---
+++ src/render.js
@@ -0,0 +1,36 @@
+const body = document.querySelector("body");
+export function createItemModal() {
+  const createItemDialog = document.createElement("dialog");
+  createItemDialog.setAttribute("class", "todoItemContainer");
+
+  body.appendChild(createItemDialog);
+  const dialogForm = document.createElement("form");
+  createItemDialog.appendChild(dialogForm);
+
+  const todoItemDetails = {
+    title: "Title:",
+    description: "Description:",
+    dueDate: "Due Date:",
+    priority: "Priority:",
+    notes: "Notes:",
+  };
+  for (const [key, value] of Object.entries(todoItemDetails)) {
+    const detailDiv = document.createElement("div");
+    detailDiv.setAttribute("id", key);
+    detailDiv.setAttribute("class", "todoFormDiv");
+    dialogForm.appendChild(detailDiv);
+    const detailLabel = document.createElement("label");
+    detailLabel.textContent = value;
+    detailDiv.appendChild(detailLabel);
+    const detailInput = document.createElement("input");
+    // detailInput.setAttribute("name", title);
+    // detailInput.setAttribute("type", text);
+    detailDiv.appendChild(detailInput);
+  }
+  const sumbitbutton = document.createElement("button");
+  sumbitbutton.setAttribute("type", "submit");
+  sumbitbutton.textContent = "Add Todo Item";
+  dialogForm.appendChild(sumbitbutton);
+
+  createItemDialog.showModal();
+}
