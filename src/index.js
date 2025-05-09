088fb114ade86d4c5e5f7575e8091f9e78adf059
Jack Butler <jackbutler.info@gmail.com>
2025-05-09 15:28:08 +0100

    pushing most recent version for simon to review

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import "./styles.css";
-import { createItemModal } from "./render";
+import {
+  createItemModal,
+  renderTodoItem,
+  renderTodoItem2,
+  renderNewTodoItemButton,
+  renderAllTodos,
+} from "./render";
 
 class TodoItem {
   constructor(id, title, description, dueDate, priority, notes) {
@@ -12,12 +18,51 @@ class TodoItem {
   }
 }
 
+// hard coded toDos
+
 const FirstTodoItem = new TodoItem(
+  1,
   "Buy shoes",
   "I am hoping to buy shoes",
-  "01/05/2025"
+  "01/05/2025",
+  "urgent",
+  "N/A"
 );
+const secondTodoItem = new TodoItem(
+  1,
+  "Buy shoes",
+  "I am hoping to buy shoes",
+  "01/05/2025",
+  "urgent",
+  "N/A"
+);
+
+const toDoList = [FirstTodoItem, secondTodoItem];
 
 console.log(FirstTodoItem);
 
-createItemModal();
+function addToArray(
+  titleParam,
+  descriptionParam,
+  dueDateParam,
+  priorityParam,
+  notesParam
+) {
+  const inputtedTodoItem = new TodoItem(
+    toDoList.length + 1,
+    titleParam,
+    descriptionParam,
+    dueDateParam,
+    priorityParam,
+    notesParam
+  );
+  toDoList.push(inputtedTodoItem);
+  console.log(toDoList);
+}
+// createItemModal();
+// renderTodoItem();
+// renderTodoItem2(FirstTodoItem);
+// renderTodoItem2(secondTodoItem);
+renderAllTodos(toDoList, addToArray, toDoList);
+
+// renderNewTodoItemButton(addToArray, toDoList);
--- src/render.js
+++ src/render.js
@@ -1,11 +1,13 @@
 const body = document.querySelector("body");
-export function createItemModal() {
-  const createItemDialog = document.createElement("dialog");
-  createItemDialog.setAttribute("class", "todoItemContainer");
+const todoItemsContainer = document.getElementById("todoItemsContainer");
 
-  body.appendChild(createItemDialog);
+export function createItemModal(addToArrayParam, toDoListParam) {
+  const createItemDiv = document.createElement("div");
+  createItemDiv.setAttribute("class", "todoItemContainer");
+
+  todoItemsContainer.appendChild(createItemDiv);
   const dialogForm = document.createElement("form");
-  createItemDialog.appendChild(dialogForm);
+  createItemDiv.appendChild(dialogForm);
 
   const todoItemDetails = {
     title: "Title:",
@@ -23,15 +25,117 @@ export function createItemModal() {
     detailLabel.textContent = value;
     detailDiv.appendChild(detailLabel);
     const detailInput = document.createElement("input");
-    // detailInput.setAttribute("name", title);
-    // detailInput.setAttribute("type", text);
+    detailInput.setAttribute("id", "inputted" + key);
+    detailInput.setAttribute("type", "text");
     detailDiv.appendChild(detailInput);
   }
-  const sumbitbutton = document.createElement("button");
-  sumbitbutton.setAttribute("type", "submit");
-  sumbitbutton.textContent = "Add Todo Item";
-  dialogForm.appendChild(sumbitbutton);
+  const sumbitButton = document.createElement("button");
+  sumbitButton.setAttribute("type", "submit");
+  sumbitButton.textContent = "Add Todo Item";
+  dialogForm.appendChild(sumbitButton);
+  sumbitButton.addEventListener("click", function (e) {
+    e.preventDefault();
+    const inputtedTitle = document.getElementById("inputtedtitle");
+    const inputtedDescription = document.getElementById("inputteddescription");
+    const inputtedDueDate = document.getElementById("inputteddueDate");
+    const inputtedPriority = document.getElementById("inputtedpriority");
+    const inputtedNotes = document.getElementById("inputtednotes");
+    addToArrayParam(
+      inputtedTitle.value,
+      inputtedDescription.value,
+      inputtedDueDate.value,
+      inputtedPriority.value,
+      inputtedNotes.value
+    );
+    createItemDiv.remove();
+    renderAllTodos(toDoListParam);
+  });
+}
+export function renderTodoItem() {
+  const createItemDiv = document.createElement("div");
+  createItemDiv.setAttribute("class", "todoItemContainer");
 
-  createItemDialog.showModal();
-  // changing for github
+  todoItemsContainer.appendChild(createItemDiv);
+  const dialogForm = document.createElement("form");
+  createItemDiv.appendChild(dialogForm);
+
+  const todoItemDetails = {
+    title: "Title:",
+    description: "Description:",
+    dueDate: "Due Date:",
+    priority: "Priority:",
+    notes: "Notes:",
+  };
+  for (const [key, value] of Object.entries(todoItemDetails)) {
+    const detailDiv = document.createElement("div");
+    detailDiv.setAttribute("id", key);
+    detailDiv.setAttribute("class", "todoFormDiv");
+    dialogForm.appendChild(detailDiv);
+    const detailLabel = document.createElement("label");
+    detailLabel.textContent = value;
+    detailDiv.appendChild(detailLabel);
+    const detailInput = document.createElement("input");
+    detailInput.setAttribute("name", key);
+    detailInput.setAttribute("type", "text");
+    detailInput.setAttribute("id", key + "input");
+    detailDiv.appendChild(detailInput);
+  }
+}
+export function renderTodoItem2(todoItem) {
+  const titleParam = todoItem.title;
+  const descriptionParam = todoItem.description;
+  const dueDateParam = todoItem.dueDate;
+  const priorityParam = todoItem.priority;
+  const notesParam = todoItem.notes;
+
+  const createItemDiv = document.createElement("div");
+  createItemDiv.setAttribute("class", "todoItemContainer");
+
+  todoItemsContainer.appendChild(createItemDiv);
+  const dialogForm = document.createElement("form");
+  createItemDiv.appendChild(dialogForm);
+
+  const todoItemDetails = [
+    ["title", "Title:", titleParam],
+    ["description", "Description:", descriptionParam],
+    ["dueDate", "Due Date:", dueDateParam],
+    ["priority", "Priority:", priorityParam],
+    ["notes", "Notes:", notesParam],
+  ];
+  for (const todoItemDetail of todoItemDetails) {
+    const detailDiv = document.createElement("div");
+    detailDiv.setAttribute("id", todoItemDetail[0]);
+    detailDiv.setAttribute("class", "todoFormDiv");
+    dialogForm.appendChild(detailDiv);
+    const detailLabel = document.createElement("label");
+    detailLabel.textContent = todoItemDetail[1];
+    detailDiv.appendChild(detailLabel);
+    const detailInput = document.createElement("input");
+    detailInput.setAttribute("name", todoItemDetail[0]);
+    detailInput.setAttribute("type", "text");
+    if (todoItem) {
+      detailInput.setAttribute("value", todoItemDetail[2]);
+      detailInput.setAttribute("readOnly", "true");
+    }
+    detailDiv.appendChild(detailInput);
+  }
+}
+
+export function renderNewTodoItemButton(addToArrayParam, toDoListParam) {
+  const newTodoItemButton = document.createElement("button");
+  newTodoItemButton.textContent = "+";
+  newTodoItemButton.setAttribute("id", "newTodoItemButton");
+  todoItemsContainer.appendChild(newTodoItemButton);
+  newTodoItemButton.addEventListener("click", function (e) {
+    newTodoItemButton.remove();
+    createItemModal(addToArrayParam, toDoListParam);
+  });
+}
+
+export function renderAllTodos(toDoListParam, addToArray, toDoList) {
+  todoItemsContainer.replaceChildren();
+  for (const toDos of toDoListParam) {
+    renderTodoItem2(toDos);
+  }
+  renderNewTodoItemButton(addToArray, toDoList);
 }
